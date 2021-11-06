3182dee35c5f29745fce1e2467081e5ed19a29ae
Imluckyayan <imluckyayan@gmail.com>
2021-11-07 03:13:53 +0530

    learned git usage and singlechildScrollview function

--- lib/Pages/loginpage.dart
+++ lib/Pages/loginpage.dart
@@ -1,50 +1,67 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Utils/Routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            'assets/images/undraw_secure_login_pdn4.png',
-            fit: BoxFit.cover,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-              child: Text('Welcome',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-              ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-            child: Column(
-              children: [
-                TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Enter Username Here', 
-                labelText: 'Username',
-
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+              child: Image.asset(
+                'assets/images/undraw_secure_login_pdn4.png',
+                fit: BoxFit.cover,
               ),
             ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter Password',
-                    labelText: 'Password',
-                    
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Welcome',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter Username',
+                      labelText: 'Username',
+                    ),
                   ),
-                )
-              ],
-            ),
-          )
-        ]
-      )
-      );
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter Password',
+                      labelText: 'Password',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.amberAccent,
+                          minimumSize: Size(220, 40)),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ]),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Pages/loginpage.dart';
+import 'package:flutter_application_1/Utils/Routes.dart';
 import 'Pages/homepage.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: GoogleFonts.lato().fontFamily,
         primaryTextTheme: GoogleFonts.latoTextTheme(),
@@ -18,8 +20,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(), //by default slash waala hi chalta ahai first
-        '/home': (context) => HomePage(),
-        '/login': (context) => LoginPage(),
+        MyRoutes.homeRoute : (context) => HomePage(),
+        MyRoutes.loginRoute : (context) => LoginPage(),
       },
     );
   }
