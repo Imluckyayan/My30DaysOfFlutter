a064a4f456bb531188ec665bdcbaa2ff3d96341a
Imluckyayan <imluckyayan@gmail.com>
2021-11-07 16:36:20 +0530

    day 6 over

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,23 +1,36 @@
+// ignore_for_file: prefer_const_constructors
+
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Lucky";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Imluckyayan"),
         backgroundColor: Colors.redAccent,
       ), // AppBar
-      body: Center(
-        child: Text(
-          "Welcome to $days days of flutter by $name",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
+      body: Column(
+        children: [
+          SizedBox(height: 150,),
+          Image.asset(
+            'assets/images/undraw_Mobile_testing_re_w7yb.png',
+            fit: BoxFit.contain,
           ),
-        ), // Container
+          Text(
+            '''This app is in Development Phase \n Made by Lucky <3''',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+        ],
       ), // Center
       drawer: Drawer(),
     ); // Scaffold
